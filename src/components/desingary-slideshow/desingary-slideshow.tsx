280fc8c8753d6b604001d01dbb94dc243e73c265
BluescreenCreator <81440569+BluescreenCreator@users.noreply.github.com>
2021-04-30 21:22:54 +0200

    slider

---
+++ src/components/desingary-slideshow/desingary-slideshow.tsx
@@ -0,0 +1,65 @@
+import { Component, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'desingary-slideshow',
+  styleUrl: 'desingary-slideshow.css',
+  shadow: true,
+})
+export class DesingarySlideshow {
+
+  render() {
+    return (
+      <Host>
+          <div id="slider">
+            <input type="radio" name="slider" id="slide1" checked>
+            <input type="radio" name="slider" id="slide2">
+            <input type="radio" name="slider" id="slide3">
+            <input type="radio" name="slider" id="slide4">
+            <div id="slides">
+                <div id="overflow">
+                  <div class="inner">
+                      <div class="slide slide_1">
+                        <div class="slide-content">
+                            <h2>Web-Design</h2>
+                            <p>Content for Slide 1</p>
+                            <p>More content</p>
+                        </div>
+                      </div>
+                      <div class="slide slide_2">
+                        <div class="slide-content">
+                            <h2>Logo-Design</h2>
+                            <p>Content for Slide 2</p>
+                        </div>
+                      </div>
+                      <div class="slide slide_3">
+                        <div class="slide-content">
+                            <h2>Banner-Ads</h2>
+                            <p>Content for Slide 3</p>
+                        </div>
+                      </div>
+                      <div class="slide slide_4">
+                        <div class="slide-content">
+                            <h2>UX-Design</h2>
+                            <p>Content for Slide 4</p>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+            </div>
+            <div id="controls">
+                <label for="slide1"></label>
+                <label for="slide2"></label>
+                <label for="slide3"></label>
+                <label for="slide4"></label>
+            </div>
+            <div id="bullets">
+                <label for="slide1"></label>
+                <label for="slide2"></label>
+                <label for="slide3"></label>
+                <label for="slide4"></label>
+            </div>
+          </div>
+      </Host>
+    );
+  }
+}
