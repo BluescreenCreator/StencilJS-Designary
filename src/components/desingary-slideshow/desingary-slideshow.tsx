a4c938074dab5c56e49304ec6c1f092407d21596
BluescreenCreator <81440569+BluescreenCreator@users.noreply.github.com>
2021-05-01 11:56:01 +0200

    slideshow

--- src/components/desingary-slideshow/desingary-slideshow.tsx
+++ src/components/desingary-slideshow/desingary-slideshow.tsx
@@ -12,52 +12,95 @@ export class DesingarySlideshow {
     return (
       <Host>
           <div id="slider">
-            <input type="radio" name="slider" id="slide1" checked></input>
-            <input type="radio" name="slider" id="slide2"></input>
-            <input type="radio" name="slider" id="slide3"></input>
-            <input type="radio" name="slider" id="slide4"></input>
+            <input type="radio" name="slider" id="slide1" checked>
+            <input type="radio" name="slider" id="slide2">
+            <input type="radio" name="slider" id="slide3">
+            <input type="radio" name="slider" id="slide4">
             <div id="slides">
                 <div id="overflow">
                   <div class="inner">
                       <div class="slide slide_1">
                         <div class="slide-content">
+                            <div class="image">
+                              <img src="slide1.png" class="picture">
+                            </div>
+                            <div class="text">
                             <h2>Web-Design</h2>
-                            <p>Content for Slide 1</p>
-                            <p>More content</p>
+                            <br>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                            <p>tempor incididunt ut labore et dolore magna aliqua erat, sed diam</p>
+                            <p>voluptua. At vero eos et accusam et justo duo.</p>
+                            <br>
+                            <br>
+                            <p style="font-weight:bold">Read more ></p>
                         </div>
+                        </div>   
                       </div>
                       <div class="slide slide_2">
                         <div class="slide-content">
+                            <div class="image">
+                              <img src="slide2.png" class="picture">
+                            </div>
+                            <div class="text">
                             <h2>Logo-Design</h2>
-                            <p>Content for Slide 2</p>
+                            <br>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                            <p>tempor incididunt ut labore et dolore magna aliqua erat, sed diam</p>
+                            <p>voluptua. At vero eos et accusam et justo duo.</p>
+                            <br>
+                            <br>
+                            <p style="font-weight:bold">Read more ></p>
+                            </div>
                         </div>
                       </div>
                       <div class="slide slide_3">
                         <div class="slide-content">
+                            <div class="image">
+                              <img src="slide3.png" class="picture">
+                            </div>
+                            <div class="text">
                             <h2>Banner-Ads</h2>
-                            <p>Content for Slide 3</p>
+                            <br>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                            <p>tempor incididunt ut labore et dolore magna aliqua erat, sed diam</p>
+                            <p>voluptua. At vero eos et accusam et justo duo.</p>
+                            <br>
+                            <br>
+                            <p style="font-weight:bold">Read more ></p>
+                            </div>
                         </div>
                       </div>
                       <div class="slide slide_4">
                         <div class="slide-content">
+                            <div class="image">
+                              <img src="slide1.png" class="picture">
+                            </div>
+                            <div class="text">
                             <h2>UX-Design</h2>
-                            <p>Content for Slide 4</p>
+                            <br>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                            <p>tempor incididunt ut labore et dolore magna aliqua erat, sed diam</p>
+                            <p>voluptua. At vero eos et accusam et justo duo.</p>
+                            <br>
+                            <br>
+                            <p style="font-weight:bold">Read more ></p>
+                            </div>
                         </div>
                       </div>
                   </div>
                 </div>
             </div>
             <div id="controls">
-                <label htmlFor="slide1"></label>
-                <label htmlFor="slide2"></label>
-                <label htmlFor="slide3"></label>
-                <label htmlFor="slide4"></label>
+                <label for="slide1"></label>
+                <label for="slide2"></label>
+                <label for="slide3"></label>
+                <label for="slide4"></label>
             </div>
             <div id="bullets">
-                <label htmlFor="slide1"></label>
-                <label htmlFor="slide2"></label>
-                <label htmlFor="slide3"></label>
-                <label htmlFor="slide4"></label>
+                <label for="slide1"></label>
+                <label for="slide2"></label>
+                <label for="slide3"></label>
+                <label for="slide4"></label>
             </div>
           </div>
       </Host>
