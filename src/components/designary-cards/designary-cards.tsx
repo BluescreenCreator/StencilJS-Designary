90fd50ace9b915b80d9eb1bc1c8fd01fa1f2f388
selina-ne <81676593+selina-ne@users.noreply.github.com>
2021-05-03 16:53:30 +0200

    designary-cards

--- src/components/designary-cards/designary-cards.tsx
+++ src/components/designary-cards/designary-cards.tsx
@@ -10,7 +10,28 @@ export class DesignaryCards {
   render() {
     return (
       <Host>
-        <slot></slot>
+        <div class="container"></div>
+        <h2 class="section-title" >Cards</h2>
+        <div class="row">
+          <div class="column">
+            <div class="card">
+            <img class="card-icon" src="src/icons/card-icon1.svg"></img>
+        <h3 >1</h3>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p> </div>
+        </div>
+          <div class="column">
+        <div class="card">
+                <img class="card-icon" src="src/icons/card-icon2.svg"></img>
+          <h3 >2</h3>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p> </div>
+      </div>
+      <div class="column">
+        <div class="card">
+                <img class="card-icon" src="src/icons/card-icon3.svg"></img>
+          <h3 >3</h3>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p> </div>
+      </div>
+    </div>  
       </Host>
     );
   }
