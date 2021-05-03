0706ba750652f321f5541a423de80f9e50c74b5f
BluescreenCreator <81440569+BluescreenCreator@users.noreply.github.com>
2021-05-03 16:02:46 +0200

    test

--- src/components/desingary-test/desingary-test.tsx
+++ src/components/desingary-test/desingary-test.tsx
@@ -11,22 +11,22 @@ export class DesingaryTest {
     return (
       <Host>
         <div class="slide slide_1">
-                        <div class="slide-content">
-                            <div class="image">
-                              <img src="slide1.png" class="picture"></img>
-                            </div>
-                            <div class="text">
-                            <h2>Web-Design</h2>
-                            <br></br>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
-                            <p>tempor incididunt ut labore et dolore magna aliqua erat, sed diam</p>
-                            <p>voluptua. At vero eos et accusam et justo duo.</p>
-                            <br></br>
-                            <br></br>
-                            <p>Read more</p>
-                        </div>
-                        </div>   
-                      </div>
+          <div class="slide-content">
+            <div class="image">
+                <img src="slide1.png" class="picture"></img>
+            </div>
+              <div class="text">
+               <h2>Web-Design</h2>
+               <br></br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                 <p>tempor incididunt ut labore et dolore magna aliqua erat, sed diam</p>
+                 <p>voluptua. At vero eos et accusam et justo duo.</p>
+                <br></br>
+                <br></br>
+                 <p>Read more</p>
+             </div>
+          </div>   
+        </div>
       </Host>
     );
   }
