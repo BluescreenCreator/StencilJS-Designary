831b4362872940984a1050886b9cc05be0a8de5f
BluescreenCreator <81440569+BluescreenCreator@users.noreply.github.com>
2021-05-04 21:52:07 +0200

    designary-contact new

--- src/components/designary-contact/desingary-contact.tsx
+++ src/components/designary-contact/desingary-contact.tsx
@@ -12,40 +12,27 @@ export class DesignaryContact {
   render() {
       return (
           <Host>
-            <div class="flex-col">
-              <h1 class="title-tagh4">Get In Touch</h1>
-              <div class="flex-row">
-                <div class="icon-1">
-                  <div class="overlap-group3">
-                    <div class="path-1 border-2px-gallery"></div>
+            <div class="contact">
+              <h1 class="getintouch">Get In Touch</h1>
+                <div class="location">
+                  <img class="locationicon" src="src/icons/location-icon.png"></img>
+                  <h2 class="locationtext">Beispoelstraße 1, 12345 Musterstadt</h2>
+                </div>
+                <div class="phone">
+                  <img class="phoneicon" src="src/icons/phone-icon.png"></img>
+                  <h2 class="phonetext">(+1) 123 456 7890</h2>
+                </div>
+                <div class="mobile">
+                  <img class="mobileicon" src="src/icons/phone-icon.png"></img>
+                  <h2 class="mobiletext">(+1) 098 765 4321</h2>
+                </div>
+                  <div class="inputs">
+                    <input class="name" type="text" placeholder="Name"></input>
+                    <input class="email" type="email" placeholder="Email Address"></input>
+                    <input class="message" type="text" placeholder="Write your Message here"></input>
                   </div>
-                </div>
-                <div class="address">Beispielstraße 1, 12345 Musterstadt</div>
+                    <button>Send</button>
               </div>
-              <div class="flex-row-3">
-                <img class="path" src='src/icons/contact-icon8.png'/>
-                <div class="phone lato-light-gallery-16px">(+1) 123 456 7890</div>
-              </div>
-              <div class="flex-row-1">
-                <div class="icon">
-                  <div class="overlap-group4 border-2px-gallery">
-                    <img class="line-1" src='src/icons/contact-icon5.png'/>
-                  </div>
-                </div>
-                <div class="mobile lato-light-gallery-16px">(+1) 098 765 4321</div>
-              </div>
-              <div class="flex-row-2">
-                <div class="input-1"><div class="value-2 lato-normal-concrete-16px">Name</div></div>
-                <div class="input"><div class="value-1 lato-normal-concrete-16px">Email Address</div></div>
-              </div>
-              <div class="textarea">
-                <div class="overlap-group2">
-                  <div class="area-displayel-ultstatedefault"></div>
-                  <div class="value">Write your message here</div>
-                </div>
-              </div>
-              <div class="button"><div class="label">Send</div></div>
-            </div>
         </Host>
       );
     }
