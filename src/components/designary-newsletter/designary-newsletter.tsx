48752f0ebdb07ae51ee2d50bdf5c69f0bb985bc4
MeliV <melissa.vela@web.de>
2021-05-04 23:19:41 +0200

    newsletter --> description-text, button

--- src/components/designary-newsletter/designary-newsletter.tsx
+++ src/components/designary-newsletter/designary-newsletter.tsx
@@ -10,10 +10,12 @@ export class DesignaryNewsletter {
   render() {
     return (
       <Host>
-        <div class="employee-container">
+        <div class="newsletter-container">
           Newsletter
+          <div class="description-text">You want to learn more about us and our services? <br></br>Just subscribe to our newsletter!</div>
           <div class="component-container"> 
-          
+            <input type="email" name="newsletter" placeholder="Email Address"></input>
+            <button>Subscribe</button>
           </div>
         </div>
       </Host>
