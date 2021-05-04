51b90e9ba718cb24a73dd7fa76c3ef63ca1997ab
BluescreenCreator <81440569+BluescreenCreator@users.noreply.github.com>
2021-05-04 14:34:50 +0200

    added slideshow

---
+++ src/components/designary-slideshow/designary-slideshow.tsx
@@ -0,0 +1,103 @@
+import { Component, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'designary-slideshow',
+  styleUrl: 'designary-slideshow.css',
+  shadow: true,
+})
+export class DesignarySlideshow {
+
+  render() {
+    return (
+      <Host>
+        <div id="slider">
+            <input type="radio" name="slider" id="slide1" checked></input>
+            <input type="radio" name="slider" id="slide2"></input>
+            <input type="radio" name="slider" id="slide3"></input>
+            <input type="radio" name="slider" id="slide4"></input>
+            <div id="slides">
+                <div id="overflow">
+                  <div class="inner">
+                      <div class="slide slide_1">
+                        <div class="slide-content">
+                            <div class="image">
+                              <img src="slide1.png" class="picture"></img>
+                            </div>
+                            <div class="text">
+                            <h2>Web-Design</h2>
+                            <br></br>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                            <p>tempor incididunt ut labore et dolore magna aliqua erat, sed diam</p>
+                            <p>voluptua. At vero eos et accusam et justo duo.</p>
+                            <br></br>
+                            <br></br>
+                            <p>Read more</p>
+                        </div>
+                        </div>   
+                      </div>
+                      <div class="slide slide_2">
+                        <div class="slide-content">
+                            <div class="image">
+                              <img src="slide2.png" class="picture"></img>
+                            </div>
+                            <div class="text">
+                            <h2>Logo-Design</h2>
+                            <br></br>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                            <p>tempor incididunt ut labore et dolore magna aliqua erat, sed diam</p>
+                            <p>voluptua. At vero eos et accusam et justo duo.</p>
+                            <br></br>
+                            <br></br>
+                            <p>Read more</p>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="slide slide_3">
+                        <div class="slide-content">
+                            <div class="image">
+                              <img src="slide3.png" class="picture"></img>
+                            </div>
+                            <div class="text">
+                            <h2>Banner-Ads</h2>
+                            <br></br>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                            <p>tempor incididunt ut labore et dolore magna aliqua erat, sed diam</p>
+                            <p>voluptua. At vero eos et accusam et justo duo.</p>
+                            <br></br>
+                            <br></br>
+                            <p>Read more</p>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="slide slide_4">
+                        <div class="slide-content">
+                            <div class="image">
+                              <img src="slide1.png" class="picture"></img>
+                            </div>
+                            <div class="text">
+                            <h2>UX-Design</h2>
+                            <br></br>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                            <p>tempor incididunt ut labore et dolore magna aliqua erat, sed diam</p>
+                            <p>voluptua. At vero eos et accusam et justo duo.</p>
+                            <br></br>
+                            <br></br>
+                            <p>Read more</p>
+                            </div>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+            </div>
+            <div id="controls">
+                <label htmlFor="slide1"></label>
+                <label htmlFor="slide2"></label>
+                <label htmlFor="slide3"></label>
+                <label htmlFor="slide4"></label>
+            </div>
+          </div>
+      </Host>
+    );
+  }
+
+}
