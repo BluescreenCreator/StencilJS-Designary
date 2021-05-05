56291ca6bfdb3d95e9b664f66a5272122c2af94a
celinerippl <celinerippl@outlook.de>
2021-05-05 16:23:32 +0200

    teaser

--- src/components/designary-teaser/designary-teaser.tsx
+++ src/components/designary-teaser/designary-teaser.tsx
@@ -5,7 +5,7 @@ import { Component, Host, h } from '@stencil/core';
   styleUrl: 'designary-teaser.css',
   shadow: true,
 })
-export class DesignaryTeaser {
+export class DesignaryParallax {
 
   render() {
     return (
@@ -17,62 +17,32 @@ export class DesignaryTeaser {
                 <ul>
                   <li>
                     <figure>
-                      <img src="bilder/delfine.jpg" alt="Delfine" class="bildDelfine" />
+                      <img src="bilder/delfine.jpg" alt="Delfine" class="bildDelfine"/>
                     </figure>
                     <div class="text">
-                      <h3>Anreise planen</h3>
+                      <h3>text</h3>
                       <p>Learn more about it</p>
-                      <br>
-                        <br>
-                          <a href="#" class="button">See more</a>
+                      <br></br>
+                      <br></br>
+                      <a href="#" class="button">See more</a>
                     </div>
-                </li>
-            </ul>
-        </div>
-                  <div class="describtionZug">
-                    <h3>Discover more!</h3>
-                    <p class="continuousText">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. </p>
-                    <p class="continuousText">Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.</p>
-                  </div>
-          </div>
-         </section>
-            </div>
-
-            <br>
-              <br>
-                <br>
-                  <br>
-
-                    <div class="main">
-                      <section>
-                        <div class="wrapper">
-                          <div class="describtion">
-                            <h3>Discover more!</h3>
-                            <p class="continuousText">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
-                            <p class="continuousText">Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
-                            <p class="continuousText">Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
-                          </div>
-                          <div class="teaser">
-                            <ul>
-                              <li>
-                                <figure>
-                                  <img src="bilder/strasse.jpg" alt="Strasse" class="bildStrasse" />
-                                </figure>
-                                <div class="text">
-                                  <h3>Anreise planen</h3>
-                                  <p>Learn more about it</p>
-                                  <br>
-                                    <br>
-                                      <a href="#" class="button">See more</a>
-                        </div>
-                    </li>
+                  </li>
                 </ul>
+              </div>
+              <div class="describtionZug">
+                <h3>Discover more!</h3>
+                <p class="continuousText">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+                <p class="continuousText">Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.</p>
+              </div>
             </div>
-           </div>
           </section>
-                        </div>
-    </Host>
+        </div>
+        <br></br>
+        <br></br>
+        <div class="main">
+          
+        </div>
+      </Host>
     );
   }
-
 }
