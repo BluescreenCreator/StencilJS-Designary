3d6efd9d53f8d7ef87e139067555d82416926847
selina-ne <81676593+selina-ne@users.noreply.github.com>
2021-05-05 16:49:28 +0200

    gallery

--- src/components.d.ts
+++ src/components.d.ts
@@ -50,8 +50,6 @@ export namespace Components {
         "person": string;
         "text": string;
     }
-    interface DesignaryText {
-    }
     interface ExampleComponent {
         "exampleProp": string;
         "exampleToUpperCase": () => Promise<void>;
@@ -168,12 +166,6 @@ declare global {
         prototype: HTMLDesignaryTestimonialElement;
         new (): HTMLDesignaryTestimonialElement;
     };
-    interface HTMLDesignaryTextElement extends Components.DesignaryText, HTMLStencilElement {
-    }
-    var HTMLDesignaryTextElement: {
-        prototype: HTMLDesignaryTextElement;
-        new (): HTMLDesignaryTextElement;
-    };
     interface HTMLExampleComponentElement extends Components.ExampleComponent, HTMLStencilElement {
     }
     var HTMLExampleComponentElement: {
@@ -203,7 +195,6 @@ declare global {
         "designary-slideshow": HTMLDesignarySlideshowElement;
         "designary-teaser": HTMLDesignaryTeaserElement;
         "designary-testimonial": HTMLDesignaryTestimonialElement;
-        "designary-text": HTMLDesignaryTextElement;
         "example-component": HTMLExampleComponentElement;
         "my-component": HTMLMyComponentElement;
     }
@@ -253,8 +244,6 @@ declare namespace LocalJSX {
         "person"?: string;
         "text"?: string;
     }
-    interface DesignaryText {
-    }
     interface ExampleComponent {
         "exampleProp"?: string;
         "onExampleEvent"?: (event: CustomEvent<string>) => void;
@@ -290,7 +279,6 @@ declare namespace LocalJSX {
         "designary-slideshow": DesignarySlideshow;
         "designary-teaser": DesignaryTeaser;
         "designary-testimonial": DesignaryTestimonial;
-        "designary-text": DesignaryText;
         "example-component": ExampleComponent;
         "my-component": MyComponent;
     }
@@ -315,7 +303,6 @@ declare module "@stencil/core" {
             "designary-slideshow": LocalJSX.DesignarySlideshow & JSXBase.HTMLAttributes<HTMLDesignarySlideshowElement>;
             "designary-teaser": LocalJSX.DesignaryTeaser & JSXBase.HTMLAttributes<HTMLDesignaryTeaserElement>;
             "designary-testimonial": LocalJSX.DesignaryTestimonial & JSXBase.HTMLAttributes<HTMLDesignaryTestimonialElement>;
-            "designary-text": LocalJSX.DesignaryText & JSXBase.HTMLAttributes<HTMLDesignaryTextElement>;
             "example-component": LocalJSX.ExampleComponent & JSXBase.HTMLAttributes<HTMLExampleComponentElement>;
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
         }
