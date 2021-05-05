36e9bec9369845b9863c5b0d0f2923c3b13dfc18
fabienne1005 <fabienne.nieft@gmx.de>
2021-05-05 19:35:15 +0200

    Button&Teaser changes

--- src/components.d.ts
+++ src/components.d.ts
@@ -53,6 +53,8 @@ export namespace Components {
         "person": string;
         "text": string;
     }
+    interface DesignaryText {
+    }
     interface ExampleComponent {
         "exampleProp": string;
         "exampleToUpperCase": () => Promise<void>;
@@ -169,6 +171,12 @@ declare global {
         prototype: HTMLDesignaryTestimonialElement;
         new (): HTMLDesignaryTestimonialElement;
     };
+    interface HTMLDesignaryTextElement extends Components.DesignaryText, HTMLStencilElement {
+    }
+    var HTMLDesignaryTextElement: {
+        prototype: HTMLDesignaryTextElement;
+        new (): HTMLDesignaryTextElement;
+    };
     interface HTMLExampleComponentElement extends Components.ExampleComponent, HTMLStencilElement {
     }
     var HTMLExampleComponentElement: {
@@ -198,6 +206,7 @@ declare global {
         "designary-slideshow": HTMLDesignarySlideshowElement;
         "designary-teaser": HTMLDesignaryTeaserElement;
         "designary-testimonial": HTMLDesignaryTestimonialElement;
+        "designary-text": HTMLDesignaryTextElement;
         "example-component": HTMLExampleComponentElement;
         "my-component": HTMLMyComponentElement;
     }
@@ -250,6 +259,8 @@ declare namespace LocalJSX {
         "person"?: string;
         "text"?: string;
     }
+    interface DesignaryText {
+    }
     interface ExampleComponent {
         "exampleProp"?: string;
         "onExampleEvent"?: (event: CustomEvent<string>) => void;
@@ -285,6 +296,7 @@ declare namespace LocalJSX {
         "designary-slideshow": DesignarySlideshow;
         "designary-teaser": DesignaryTeaser;
         "designary-testimonial": DesignaryTestimonial;
+        "designary-text": DesignaryText;
         "example-component": ExampleComponent;
         "my-component": MyComponent;
     }
@@ -309,6 +321,7 @@ declare module "@stencil/core" {
             "designary-slideshow": LocalJSX.DesignarySlideshow & JSXBase.HTMLAttributes<HTMLDesignarySlideshowElement>;
             "designary-teaser": LocalJSX.DesignaryTeaser & JSXBase.HTMLAttributes<HTMLDesignaryTeaserElement>;
             "designary-testimonial": LocalJSX.DesignaryTestimonial & JSXBase.HTMLAttributes<HTMLDesignaryTestimonialElement>;
+            "designary-text": LocalJSX.DesignaryText & JSXBase.HTMLAttributes<HTMLDesignaryTextElement>;
             "example-component": LocalJSX.ExampleComponent & JSXBase.HTMLAttributes<HTMLExampleComponentElement>;
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
         }
--- src/components/designary-teaser/designary-teaser.tsx
+++ src/components/designary-teaser/designary-teaser.tsx
@@ -11,7 +11,7 @@ export class DesignaryParallax {
     return (
       <Host>
         <div class="teasercontainer">
-          <img src="src/bilder/delfine.jpg" alt="Delfine" class="bildDelfine"></img>
+          <img src="src/bilder/delfine_teaser.jpg" alt="Delfine" class="bildDelfine"></img>
           <div class="teasertext">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</div>
           <designary-button title="See more"></designary-button>
         </div>
