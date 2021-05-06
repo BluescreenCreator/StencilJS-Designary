979926d63a03835a04f29bae833b431c9ee1d555
MeliV <melissa.vela@web.de>
2021-05-06 17:13:10 +0200

    designary-button is now part of designary-newsletter

--- src/components/designary-newsletter/designary-newsletter.tsx
+++ src/components/designary-newsletter/designary-newsletter.tsx
@@ -15,7 +15,7 @@ export class DesignaryNewsletter {
           <div class="description-text">You want to learn more about us and our services? <br></br>Just subscribe to our newsletter!</div>
           <div class="component-container"> 
             <input type="email" name="newsletter" placeholder="Email Address"></input>
-            <button>Subscribe</button>
+            <designary-button title="Subscribe"></designary-button>
           </div>
         </div>
       </Host>
